f4b60d0cb303cbd43fe0db6752a5d16844986582
Bruno Ferreira <brunobrunobrunoferreira@hotmail.com>
2022-11-03 19:01:47 +0000

    done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.lenght = this.items.lenght;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+    throw new Error('OutOfBounds');
+  }else {
+    return this.items[pos];
+  }
+  }
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
 
-  max() {}
+  }
 
-  min() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
